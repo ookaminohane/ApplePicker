a1e2b59ec66ab5f6f754469c775b01acea0f4cc5
Cypress Rhodes <54965061+ookaminohane@users.noreply.github.com>
2020-02-07 20:21:09 -0500

    project
    project. final

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AppleTree: MonoBehaviour{
+public class AppleTree: MonoBehaviour
+{
     
     public GameObject applePrefab;
     public float speed = 20f;
